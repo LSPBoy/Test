8e77cc9513d3f880b38e9bf91e87e962e6afd5b7
liji <liji@123.com>
2023-08-02 17:53:31 +0800

    提交

---
+++ Test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by apple on 2023/8/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Test/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by apple on 2023/8/2.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
